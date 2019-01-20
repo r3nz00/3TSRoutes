d5999cbc3cb2ee33d562c14ff549aabb3d4ce1f2
Deweertd <deweertd1830@gmail.com>
2019-01-20 11:29:01 -0700

    Removd add button cause crunch time

--- app/src/main/java/hacking101/a3tsroutes/AddRoute.java
+++ app/src/main/java/hacking101/a3tsroutes/AddRoute.java
@@ -1,20 +1,46 @@
 package hacking101.a3tsroutes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class AddRoute extends AppCompatActivity {
+    private Button button;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_route);
 
+        button = (Button) findViewById(R.id.AddRouteButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                checkForRoute(v);
+            }
+        });
 
     }
 
+
+    public void checkForRoute(View v){
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.AddRouteTextField);
+        String message = editText.getText().toString();
+//        Route route;
+//        if((route = new Route(message))){
+//            intent.putExtra(EXTRA_MESSAGE, message);
+//            startActivity(intent);
+//        }
+
+    }
 }
